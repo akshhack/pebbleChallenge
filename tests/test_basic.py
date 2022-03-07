7df4df93e71af407f42b04655bc7cf349f398d2b
akshhack <akshatp.cs@gmail.com>
2022-03-07 10:10:14 -0800

    added testing infra

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
-from api.models import db, Person
+from api.models import db, User
+
 
 # client passed from client - look into pytest for more info about fixtures
 # test client api: http://flask.pocoo.org/docs/1.0/api/#test-client
@@ -8,19 +9,19 @@ def test_index(client):
 
 
 def test_get_person(client):
-    rs = client.get("/persons")
+    rs = client.get("/users")
 
     assert rs.status_code == 200
     ret_dict = rs.json  # gives you a dictionary
     assert ret_dict["success"] == True
-    assert ret_dict["result"]["persons"] == []
+    assert ret_dict["result"]["users"] == []
 
-    # create Person and test whether it returns a person
-    temp_person = Person(name="Tim")
-    db.session.add(temp_person)
+    # create User and test whether it returns a person
+    temp_user = User(name="Tim", dob="02/01/1998", zip="94085")
+    db.session.add(temp_user)
     db.session.commit()
 
-    rs = client.get("/persons")
+    rs = client.get("/users")
     ret_dict = rs.json
-    assert len(ret_dict["result"]["persons"]) == 1
-    assert ret_dict["result"]["persons"][0]["name"] == "Tim"
+    assert len(ret_dict["result"]["users"]) == 1
+    assert ret_dict["result"]["users"][0]["name"] == "Tim"
